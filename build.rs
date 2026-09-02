2a2cfb3332086aad6208bba7e62c7dd3cc0e0046
NullDev (Shadow) <chris@nulldev.org>
2026-09-02 07:11:07 +0000

    prep v2

--- build.rs
+++ build.rs
@@ -2,35 +2,41 @@ use std::{env, fs};
 extern crate embed_resource;
 extern crate toml;
 fn main() {
-    embed_resource::compile("./resources/client.rc", embed_resource::NONE)
-        .manifest_optional()
-        .ok();
-    if let Err(e) = embed_resource::compile("./resources/glorp-manifest.rc", embed_resource::NONE).manifest_required() {
-        eprintln!("{}", e)
-    };
+    // target comes from env, not cfg!()
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
 
     let toml_content = fs::read_to_string("Cargo.toml").unwrap();
     let toml: toml::Value = toml::from_str(&toml_content).unwrap();
-    let package_version = toml["package"]["version"].as_str().unwrap();
     let js_bundle_version = toml["package"]["metadata"]["js_bundle_version"].as_str().unwrap();
 
+    // needed on both platforms
     let dest_path = env::current_dir().unwrap().join("target/bundle_version");
     fs::write(dest_path, js_bundle_version).unwrap();
 
-    let wxs_path = "resources/installer_script.wxs";
-    let wxs_content = fs::read_to_string(wxs_path).unwrap();
-    let regex = regex::Regex::new(r#"Version="([^"]+)""#).unwrap();
+    if target_os == "windows" {
+        embed_resource::compile("./resources/client.rc", embed_resource::NONE)
+            .manifest_optional()
+            .ok();
+        if let Err(e) = embed_resource::compile("./resources/glorp-manifest.rc", embed_resource::NONE).manifest_required() {
+            eprintln!("{}", e)
+        };
 
-    let current_version = regex
-        .captures(&wxs_content)
-        .and_then(|cap| cap.get(1))
-        .map(|m| m.as_str());
+        let package_version = toml["package"]["version"].as_str().unwrap();
+        let wxs_path = "resources/installer_script.wxs";
+        let wxs_content = fs::read_to_string(wxs_path).unwrap();
+        let regex = regex::Regex::new(r#"Version="([^"]+)""#).unwrap();
 
-    if current_version != Some(package_version) {
-        let updated_wxs = regex
-            .replace(&wxs_content, format!(r#"Version="{}""#, package_version))
-            .to_string();
-        fs::write(wxs_path, updated_wxs).unwrap();
+        let current_version = regex
+            .captures(&wxs_content)
+            .and_then(|cap| cap.get(1))
+            .map(|m| m.as_str());
+
+        if current_version != Some(package_version) {
+            let updated_wxs = regex
+                .replace(&wxs_content, format!(r#"Version="{}""#, package_version))
+                .to_string();
+            fs::write(wxs_path, updated_wxs).unwrap();
+        }
     }
 
     println!("cargo:rerun-if-changed=Cargo.toml");
